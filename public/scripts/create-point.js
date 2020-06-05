0d9018e5733353ff3dfa3ee77483ba278c16e84c
espezzialy <espezzialy.souza-ext@ab-inbev.com>
2020-06-05 15:21:09 -0300

    :rocket: Finalizado.

--- public/scripts/create-point.js
+++ public/scripts/create-point.js
@@ -1,84 +1,104 @@
 function populateUFs() {
-    const ufSelect = document.querySelector('select[name=uf]');
-    fetch('https://servicodados.ibge.gov.br/api/v1/localidades/estados')
-      .then( res => res.json() )
-      .then((states) => {
-        for (const state of states) {
-          ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
-        }
-      });
-  }
-  
-  populateUFs();
-  
-  function getCities(event) {
-    const citySelect = document.querySelector("select[name=city]");
-    const stateInput = document.querySelector("input[name=state]");
-  
-    const ufValue = event.target.value;
-  
-    const indexOfSelectedState = event.target.selectedIndex;
-    stateInput.value = event.target.options[indexOfSelectedState].text;
-  
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
-    
+  const ufSelect = document.querySelector("select[name=uf]")
 
-    citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
-    citySelect.disabled = false;
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+  .then( res => res.json() )
+  .then( states => {
 
-    fetch(url)
-      .then((res) => res.json())
-      .then((cities) => {
-        
-        for (const city of cities) {
-          citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
-        }
-  
-        citySelect.disabled = false;
-      });
-  }
-  
-  document
-    .querySelector('select[name=uf]')
-    .addEventListener('change', getCities);
+      for( const state of states ) {
+          ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
+      }
 
-    const itemsTocCollect = document.querySelectorAll(".items-grid li");
+  })
+}
 
-    for (const item of itemsTocCollect){
-      item.addEventListener("click", handleSelectedItem)
-    }
+populateUFs()
 
-    const collectedItems = document.querySelector("input[name=items]")
 
-    let selectedItems = []
+function getCities(event) {
+  const citySelect = document.querySelector("[name=city]")
+  const stateInput = document.querySelector("[name=state]")
 
-    function handleSelectedItem(event){
-      const itemLi = event.target
+  const ufValue = event.target.value
 
-      itemLi.classList.toggle("selected")
+  const indexOfSelectedState = event.target.selectedIndex
+  stateInput.value = event.target.options[indexOfSelectedState].text
 
-      const itemId = itemLi.dataset.id
 
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
+
+
+  citySelect.innerHTML = "<option value>Selecione a Cidade</option>"
+  citySelect.disabled = true
+
+  fetch(url)
+  .then( res => res.json() )
+  .then( cities => {
       
 
-      const alreadySelected = selectedItems.findIndex( item => {
-        const itemFound = item == itemId //true or false
-        return itemFound 
+      for( const city of cities ) {
+          citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`
+      }
+
+      citySelect.disabled = false
+  } )
+}
+
+
+document
+  .querySelector("select[name=uf]")
+  .addEventListener("change", getCities)
+
+
+// Itens de coleta
+// pegar todos os li`s
+const itemsToCollect = document.querySelectorAll(".items-grid li")
+
+for (const item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem)
+}
+
+
+const collectedItems = document.querySelector("input[name=items]")
+
+let selectedItems = []
+
+function handleSelectedItem(event) {
+  const itemLi = event.target
+
+  // adicionar ou remover uma classe com javascript
+  itemLi.classList.toggle("selected")
+  
+  const itemId = itemLi.dataset.id
+
+  // console.log('ITEM ID: ', itemId)
+
+  // verificar se existem itens selecionados, se sim
+  // pegar os itens selecionados
+
+  const alreadySelected = selectedItems.findIndex( item => {
+      const itemFound = item == itemId // isso será true ou false
+      return itemFound
+  })
+
+  // se já estiver selecionado, 
+  if( alreadySelected >= 0 ) {
+      // tirar da selecao
+      const filteredItems = selectedItems.filter( item => {
+          const itemIsDifferent = item != itemId // false
+          return itemIsDifferent
       })
 
-      if(alreadySelected >= 0){
-        const filteredItems = selectedItems.filter(item => {
-          const itemIsDiferent = item != itemId
-          return itemIsDiferent
-        })
+      selectedItems = filteredItems
+  } else {
+      // se não estiver selecionado
+      // adicionar à seleção
+      selectedItems.push(itemId)
+  }
 
-        selectedItems = filteredItems
-      } else {
-        selectedItems.push(itemId)
-      }
-      
-      // add or remove a class with js
-      collectedItems.value = selectedItems
-      
+  // console.log('selectedItems: ', selectedItems)
 
-    }
+  // atualizar o campo escondido com os itens selecionados
+  collectedItems.value = selectedItems
+
+}
--- src/server.js
+++ src/server.js
@@ -1,26 +1,91 @@
-const express = require("express");
-const server = express()
+const express = require("express")
+    const server = express()
 
-//configurar pasta publica
-server.use(express.static("public"))
+    const db = require("./database/db")
 
-//utilizando template engines
-const nunjucks = require("nunjucks")
-nunjucks.configure("src/views", {
-    express: server,
-    noCache: true
-})
+    server.use(express.static("public"))
 
-server.get("/", (req, res) => {
-    res.render("index.html")
-})
+    server.use(express.urlencoded({ extended: true }))
 
-server.get("/create-point", (req, res) => {
-    res.render("create-point.html")
-})
 
-server.get("/search", (req, res) => {
-    res.render("search-results.html")
-})
+    const nunjucks = require("nunjucks")
+    nunjucks.configure("src/views", {
+        express: server,
+        noCache: true
+    })
 
-server.listen(3000);
+    server.get("/", (req, res) => {
+        return res.render("index.html", { title: "Um título"})
+    })
+
+
+
+    server.get("/create-point", (req, res) => {
+
+        return res.render("create-point.html")
+    })
+
+    server.post("/savepoint", (req, res) => {
+
+        const query = `
+            INSERT INTO places (
+                image,
+                name,
+                address,
+                address2,
+                state,
+                city,
+                items
+            ) VALUES (?,?,?,?,?,?,?);
+        `
+
+        const values = [
+            req.body.image,
+            req.body.name,
+            req.body.address,
+            req.body.address2,
+            req.body.state,
+            req.body.city,
+            req.body.items
+        ]
+
+        function afterInsertData(err) {
+            if(err) {
+                console.log(err)
+                return res.send("Erro no cadastro!")
+            }
+
+            console.log("Cadastrado com sucesso")
+            console.log(this)
+
+            return res.render("create-point.html", {saved: true})
+        }
+
+        db.run(query, values, afterInsertData)
+
+    })
+
+
+
+    server.get("/search", (req, res) => {
+
+        const search = req.query.search
+
+        if(search == "") {
+            return res.render("search-results.html", { total: 0})
+        }
+
+        
+        db.all(`SELECT * FROM places WHERE city LIKE '%${search}%'`, function(err, rows) {
+            if(err) {
+                return console.log(err)
+            }
+
+            const total = rows.length
+
+            return res.render("search-results.html", { places: rows, total: total})
+        })
+    })
+
+
+    server.listen(3000)
